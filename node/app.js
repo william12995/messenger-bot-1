74b4e05c7cbef40e44bf5a2c30179eb4b369c8be
william12995 <rino881209@gmail.com>
2017-02-11 01:52:26 +0800

    test

--- node/app.js
+++ node/app.js
@@ -64,6 +64,8 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+
+    console.log(req.query['hub.verify_token']);
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {

3d399a8670d3e68c4e665d4b1c038eaf9eff81d8
william12995 <rino881209@gmail.com>
2017-02-10 17:42:31 +0800

    PORT

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var conf = module.exports = {
     FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
-    VERIFY_TOKEN: 'verify token here',
-    PROFILE_TOKEN: 'page access token here',
-    PORT: '3000'
+    VERIFY_TOKEN: 'inforchat',
+    PROFILE_TOKEN: 'EAAUnePl06eUBAIS5FDi1HNdZBbncshciWCI1KkUtxhIXd4cxDLtZCdMMxQ3c029uW6sZBPZA8lJ7Elc0ZC3tLoiNq9662zSqmWm5iMC1I8AaWHpXZCl78AjOZAN7AlPz5iyFdpkxlXv90Q4BHpYfUboAgECQAZCsqCDbNMogpsEK0wZDZD',
+    PORT: '1209'
 };
